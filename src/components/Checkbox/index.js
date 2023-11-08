6d33bcee64526a0f257edaf15b6b1d51093abfec
Iulik <inicula1989@gmail.com>
2023-11-08 18:35:18 +0200

    first commit

---
+++ src/components/Checkbox/index.js
@@ -0,0 +1,17 @@
+import './style.css'
+
+const Checkbox = ({value, toggle}) => {
+
+    const handleChange = () => {
+        toggle(!value)
+    }
+
+    return <input
+     className='checkbox'
+      type='checkbox'
+       value={value}
+        onChange={handleChange}
+         />
+}
+
+export default Checkbox
